9eefadbe74708d587845f09a1fa738c803ba85d8
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-06-04 20:54:38 +0300

    Fixed the overlapping of the navigation bar and the masthead header

--- angular-music-streaming-application/src/app/content/layout/navigation-bar/navigation-bar.component.ts
+++ angular-music-streaming-application/src/app/content/layout/navigation-bar/navigation-bar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationBarComponent implements OnInit {
 
   projectLogoPath!: String;
+  isCollapsed!: Boolean;
 
   constructor() {
   }
